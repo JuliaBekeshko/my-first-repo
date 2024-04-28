14145c74347c2c12d7d06057fea813f38510f029
JuliaBekeshko <jlbekeshko@gmail.com>
2024-04-28 23:05:21 +0300

    new project

---
+++ Lab3projectOPIPTZ/Lab3projectOPIPTZ/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Lab3projectOPIPTZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.radiusTextBox = new System.Windows.Forms.TextBox();
+            this.angleTextBox = new System.Windows.Forms.TextBox();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(419, 366);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(789, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Знайти довжину дуги кола, якщо відомі радіус кола та центральний кут, який вона о" +
+    "хоплює.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(560, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Введіть дані:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(497, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Радіус кола:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(497, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 22);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Центральний кут:";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultLabel.Location = new System.Drawing.Point(497, 369);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 20);
+            this.resultLabel.TabIndex = 6;
+            this.resultLabel.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // radiusTextBox
+            // 
+            this.radiusTextBox.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.radiusTextBox.Location = new System.Drawing.Point(659, 141);
+            this.radiusTextBox.Name = "radiusTextBox";
+            this.radiusTextBox.Size = new System.Drawing.Size(100, 22);
+            this.radiusTextBox.TabIndex = 7;
+            // 
+            // angleTextBox
+            // 
+            this.angleTextBox.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.angleTextBox.Location = new System.Drawing.Point(659, 174);
+            this.angleTextBox.Name = "angleTextBox";
+            this.angleTextBox.Size = new System.Drawing.Size(100, 22);
+            this.angleTextBox.TabIndex = 8;
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.CalculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CalculateButton.Location = new System.Drawing.Point(647, 232);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(112, 39);
+            this.CalculateButton.TabIndex = 9;
+            this.CalculateButton.Text = "Знайти";
+            this.CalculateButton.UseVisualStyleBackColor = false;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(885, 450);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.angleTextBox);
+            this.Controls.Add(this.radiusTextBox);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.TextBox radiusTextBox;
+        private System.Windows.Forms.TextBox angleTextBox;
+        private System.Windows.Forms.Button CalculateButton;
+    }
+}
+
---
+++ Lab3projectOPIPTZ/Lab3projectOPIPTZ/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3projectOPIPTZ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Отримання значень з TextBox
+                double radius = double.Parse(radiusTextBox.Text);
+                double angleDegrees = double.Parse(angleTextBox.Text);
+
+                // Переведення кута в радіани
+                double angleRadians = angleDegrees * Math.PI / 180.0;
+
+                // Обчислення довжини дуги
+                double arcLength = radius * angleRadians;
+
+                // Виведення результату
+                resultLabel.Text = $"Довжина дуги кола: {arcLength:F2}";
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Будь ласка, введіть коректні значення для радіусу та кута.");
+            }
+        }
+    }
+}
